11951cba36cfee1ae5d6c0723fa8b332e4e357d6
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-08-01 00:41:53 -0300

    Trabalhando com variaveis

---
+++ aula02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace aula02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Aula 02 - Trabalhando com Variáveis");
+      int idade = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine(idade);
+    }
+  }
+}
