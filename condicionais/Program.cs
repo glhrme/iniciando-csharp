2873afa28ca53f151d324702cf04df6ea1e248c0
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-08-01 02:36:08 -0300

    condicionais

---
+++ condicionais/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace condicionais
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Trabalhando com condicionais");
+    }
+  }
+}
