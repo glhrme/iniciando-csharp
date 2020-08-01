5265ae5597a142a9b1d4e7104b32eca09e7fd404
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-08-01 01:20:19 -0300

    Tipos de variáveis

--- aula02/Program.cs
+++ aula02/Program.cs
@@ -7,8 +7,10 @@ namespace aula02
     static void Main(string[] args)
     {
       Console.WriteLine("Aula 02 - Trabalhando com Variáveis");
-      int idade = Convert.ToInt32(Console.ReadLine());
-      Console.WriteLine(idade);
+      Console.WriteLine("Digite um inteiro");
+      int variavel1 = Convert.ToInt32(Console.ReadLine());
+      double variavel2 = 1.2;
+      Console.WriteLine(variavel1 + "/" + variavel2);
     }
   }
 }
